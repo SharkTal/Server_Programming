757811d9d119693bfed9d4ee6ab19a3637bc7252
SharkTal <47451399+SharkTal@users.noreply.github.com>
2020-02-11 09:45:38 +0200

    Add files via upload

---
+++ bookStore/Book.java
@@ -0,0 +1,67 @@
+package com.example.bookStore.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;	
+	private String author, title, isbn, year;
+
+	public Book() {
+	}
+
+	public Book(String author, String title, String isbn, String year) {
+		super();
+		this.author = author;
+		this.title = title;
+		this.isbn = isbn;
+		this.year = year;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+}
